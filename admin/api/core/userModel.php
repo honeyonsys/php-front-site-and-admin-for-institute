489642b10bb505c897d4875040e4640f17a828bd
priyankamakar31 <priyankamaakar@gmail.com>
2023-04-12 21:22:59 +0530

    time span increases done

--- admin/api/core/userModel.php
+++ admin/api/core/userModel.php
@@ -212,7 +212,7 @@ class Users
     }
 
     public function generateTokenForEmail($email) {
-        $timeStamp = time() + 900; //adding 30 more minutes
+        $timeStamp = time() + 3600; //adding 30 more minutes
         $tokenString = $email.",". $timeStamp;
         $encodedString = base64_encode($tokenString);
         $updateQry = 'UPDATE ' .$this->table. ' SET token = :token, tokenExpire = :tokenExpire WHERE email = :email';
