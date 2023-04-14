7b7b06c2c57176c1b18c72e01ad5892600930864
priyankamakar31 <priyankamaakar@gmail.com>
2023-04-14 10:08:26 +0530

    done

--- admin/assets/js/constants.js
+++ admin/assets/js/constants.js
@@ -1,8 +1,13 @@
 const apiHost = 'api';
+// const apiHost = 'https://harksh.com/ims/admin/api';
 const apiLoginUser = apiHost + '/user_login.php';
 const apiLogoutUser = apiHost + '/user_logout.php';
 const apiVerifyToken = apiHost + '/verify_token.php';
 const apiPostCreate = apiHost + '/post_create.php';
 const apiPostReadAll = apiHost + '/post_readall.php';
+
+
+// local storage session name
 const sessionEmail = "ims-session-email";
 const sessionToken = "ims-session-token";
+const apiAttendanceCreate=apiHost+'/attendance_create.php';
