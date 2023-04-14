16deab7807cfe585049d517b246586bb0278d139
honeyonsys@gmail.com <honeyonsys@gmail.com>
2023-04-14 10:06:33 +0530

    attendance code in front

--- admin/assets/js/constants.js
+++ admin/assets/js/constants.js
@@ -4,5 +4,6 @@ const apiLogoutUser = apiHost + '/user_logout.php';
 const apiVerifyToken = apiHost + '/verify_token.php';
 const apiPostCreate = apiHost + '/post_create.php';
 const apiPostReadAll = apiHost + '/post_readall.php';
+const apiAttendanceCreate = apiHost + '/attendance_create.php';
 const sessionEmail = "ims-session-email";
 const sessionToken = "ims-session-token";
