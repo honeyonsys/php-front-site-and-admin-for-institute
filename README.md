b4b7f47f046af97b2775f2a9490218e0796be578
honeyonsys@gmail.com <honeyonsys@gmail.com>
2023-04-15 15:26:11 +0530

    revamp the front website design

--- README.md
+++
@@ -1,2 +0,0 @@
-# php-front-site-and-admin-for-institute
-A very basic landing page built in php to show about some institute, its notifications, news, attendance, student login, teacher login, also an admin to manage content
--- project/assets/js/constants.js
+++
@@ -1,6 +0,0 @@
-const apiHost = 'api';
-const apiLoginUser = apiHost + '/user_login.php';
-const apiLogoutUser = apiHost + '/user_logout.php';
-const apiVerifyToken = apiHost + '/verify_token.php';
-const sessionEmail = "ims-session-email";
-const sessionToken = "ims-session-token";
